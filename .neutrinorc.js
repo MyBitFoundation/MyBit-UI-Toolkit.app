8dc6511db2c4bf520b8686199115d8cbb4888b17
Ivan Ivanov <ivan.milchev.ivanov@gmail>
2018-09-25 16:04:27 +0300

    hotfix: disable mangle

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -9,6 +9,13 @@ module.exports = {
     }],
     '@neutrinojs/jest',
     ['@neutrinojs/react', {
+      minify: {
+        babel: {
+          minify: {
+            mangle: false
+          }
+        }
+      },
       html: {
         title: 'MyBit - UI Kit'
       },
