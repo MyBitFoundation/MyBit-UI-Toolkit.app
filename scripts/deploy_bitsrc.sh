fb0d086ee701f6c9d9b932efa7af697d57e8a748
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-11-21 07:34:44 -0600

    Added suggestions obtained in Gitter

--- scripts/deploy_bitsrc.sh
+++ scripts/deploy_bitsrc.sh
@@ -3,17 +3,15 @@
 npm set init.author.name "MyBit AG"
 npm set init.author.email "mybit.development@gmail.com"
 npm config set @bit:registry = "https://node.bitsrc.io"
-echo "//registry.npmjs.org/:_authToken=$NPM_MYBIT_BOT_TOKEN" > ~/.npmrc
-echo "//node.bitsrc.io/:_authToken=$NPM_MYBIT_BOT_TOKEN" >> ~/.npmrc
+echo "//node.bitsrc.io/:_authToken=$NPM_MYBIT_BOT_TOKEN" > ~/.npmrc
 bit config set analytics_reporting false
 bit config set error_reporting false
 bit config set user.name jjperezaguinaga
 bit config set user.email me@jjperezaguinaga.com
 bit config set ssh_key_file bit_rsa
 bit init
-bit import mybit.ui
+bit import
 bit status
-bit build
 bit tag --all --patch
 bit export mybit.ui
 
