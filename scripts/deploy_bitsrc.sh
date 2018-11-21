56d41c4647b7673c7880e9352ed66bd8d70686bc
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-11-20 21:24:53 -0600

    Upgrading gitignore to not include vim swap files and travis build to execute all bitsrc steps as a separate script

---
+++ scripts/deploy_bitsrc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+npm set init.author.name "MyBit AG"
+npm set init.author.email "mybit.development@gmail.com"
+npm config set @bit:registry = "https://node.bitsrc.io"
+echo "//registry.npmjs.org/:_authToken=$NPM_MYBIT_BOT_TOKEN" > ~/.npmrc
+echo "//node.bitsrc.io/:_authToken=$NPM_MYBIT_BOT_TOKEN" >> ~/.npmrc
+bit config set analytics_reporting false
+bit config set error_reporting false
+bit config set user.name jjperezaguinaga
+bit config set user.email me@jjperezaguinaga.com
+bit config set ssh_key_file bit_rsa
+bit init
+bit add src/showcase/*
+bit tag --all
+bit export mybit.ui
+
