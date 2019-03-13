78e858e434964ac36338189f37793e78558e25af
Bhanu Teja <pbteja1998@gmail.com>
2019-03-14 01:25:41 +0530

    add documentation for adding new component

--- packages/App/src/components/Pages/ContributePage.js
+++ packages/App/src/components/Pages/ContributePage.js
@@ -4,9 +4,13 @@ import { Row, Col } from 'antd'
 import { Helmet } from "react-helmet";
 import { CONTRIBUTE_TITLE } from './pageTitles'
 
-const GIT_REPO_LINK = "https://github.com/MyBitFoundation/MyBit-UI.website"
-const STYLED_COMPONENTS_LINK = "https://www.styled-components.com/docs"
-const EDIT_PAGE = "https://github.com/MyBitFoundation/MyBit-UI.website/blob/develop/src/components/Pages/ContributePage.js"
+const GIT_REPO_LINK = "https://github.com/MyBitFoundation/MyBit-UI.website";
+const STYLED_COMPONENTS_LINK = "https://www.styled-components.com/docs";
+const STORYBOOK_LINK = "https://storybook.js.org/docs/basics/introduction/";
+const STORYBOOK_ADDONS_LINK = "https://storybook.js.org/addons/";
+const BABEL_LINK = "https://babeljs.io/docs/en/";
+const EDIT_PAGE = "https://github.com/MyBitFoundation/MyBit-UI.website/blob/develop/src/components/Pages/ContributePage.js";
+const ORG_NAME = "@mybit";
 
 const ContributePage = () => (
     <div>
@@ -15,34 +19,49 @@ const ContributePage = () => (
         </Helmet>
 
         <h1>How to contribute? <EditPageLink editLink={EDIT_PAGE} /></h1>
+
         <h4>1. Fork the main repository on GitHub <a href={GIT_REPO_LINK}>here</a></h4>
+
         <h4>2. Clone the forked repository on your machine</h4>
         <PlainCode>git clone https://github.com/--your-username--/MyBit-UI.website</PlainCode>
 
-        <h4>3. Create a folder for an isolated component</h4>
+        <h4>3. Copy the Sample repo into a folder in packages/ for an isolated component</h4>
+        <PlainCode>cp -R Sample packages/--your-new-component-folder--</PlainCode>
+
+        <h4>4. Change the package name of the new component</h4>
         <p>
-           Create a new folder <CustomTag>src/showcase/--your-component-folder--</CustomTag> and an <CustomTag>index.js</CustomTag> file to contain your code.
-           All styles must be in the same folder. We use styled-components to allow advanced customization options - <a href={STYLED_COMPONENTS_LINK}>read their documantion</a>.
+            Open <CustomTag>packages/--your-new-component-folder--/package.json</CustomTag> and change name to <CustomTag>{ORG_NAME + "/ui.kit.--new-component-name--"}</CustomTag>.
         </p>
 
-        <h4>4. Export your components</h4>
-        <p>
-            Export new component from <CustomTag>src/showcase/index.js</CustomTag>
-        </p>
-        <PlainCode>{`export { default as --your-component-name--} from './--your-component-folder--'`}</PlainCode>
+        <p>Basic babel setup is made. If any more plugins are needed for babel, add them in <CustomTag>.babelrc</CustomTag> - <a href={BABEL_LINK}>read their documentation</a>.</p>
 
-        <h4>5. Create the component page</h4>
+        <p>All styles must be in the same folder as <CustomTag>packages/--your-new-component-folder--</CustomTag>. We use styled-components to allow advanced customization options - <a href={STYLED_COMPONENTS_LINK}>read their documentation</a>.</p>
+
         <p>
-           Create a folder <CustomTag>src/components/Documentation/--your-documentation-folder--</CustomTag> with an index file to contain the documentation for your component.
+            Add stories for the component in <CustomTag>packages/index.stories.js</CustomTag>.
+            See other stories in the file for a basic idea to write story for the new component - <a href={STORYBOOK_LINK}>read their documentation</a>.
         </p>
 
-        <h4>6. Create a route to your documentation page</h4>
+        <p>Also checkout <a href={STORYBOOK_ADDONS_LINK}>official storybook addons</a> for advanced testing of component using storybook.</p>
+
+        <h4>5. For adding a local component package as a dependency to the new package, run the following command</h4>
+        <PlainCode>lerna add {ORG_NAME}/ui.kit.old-component --scope={ORG_NAME}/ui.kit.--new-component-name-- </PlainCode>
+        <p>Remote npm packages can be added normally to the new package using <CustomTag>npm install --package-name--</CustomTag> after navigating to the new component folder.</p>
+        <p>Any packages that are present in the root's <CustomTag>package.json</CustomTag> can be used by all the packages that are in <CustomTag>packages</CustomTag> folder.</p>
+
+        <h4>6. Create the component page</h4>
+        <p>
+            Create a folder <CustomTag>packages/App/src/components/Documentation/--your-documentation-folder--</CustomTag> with an index file to contain the documentation for your component.
+        </p>
+
+        <h4>7. Create a route to your documentation page</h4>
         <p>Import the new component in <CustomTag>src/components/Documentation/index.js</CustomTag></p>
         <PlainCode>{`import --your-component-name--Page from './--your-documentation-folder--'`}</PlainCode>
 
         <p>Add an entry to the array <CustomTag>documentationRoutes</CustomTag> in the same file as above</p>
         <PlainCode>
-{`{
+            {
+                `{
     title: --your-component-name--,
     url: --your-component-name--,
     component: --your-component-name--Page,
@@ -53,15 +72,14 @@ const ContributePage = () => (
 Categories:
 GENERAL, FEEDBACK, NAVIGATION, FORMS, ASSETS,
 USER, WIDGETS, TEMPLATES, LIBRARY_COMPONENTS`
-}
-    </PlainCode>
+            }
+        </PlainCode>
 
-        <h4>7. Create a pull request to the main repository</h4>
+        <h4>8. Create a pull request to the main repository</h4>
 
-        <h1>Example code for a documentation page</h1>
         <Showcase data={contributeExample} />
     </div>
-)
+);
 
 
 const mycode = `
@@ -126,7 +144,7 @@ const temlateData = [{
     required: 'true/false',
     default: '-'
 }];
-`
+`;
 
 const temlateData = [{
     key: '1',
@@ -148,16 +166,16 @@ const templateExample = {
     ),
     code: '//code',
     display: 'inline-block'
-}
+};
 
-const EDIT_PAGE_LINK = "/"
+const EDIT_PAGE_LINK = "/";
 
 const TEMPLATE_PAGE = (
     <div style={{width: '90%'}}>
         <h1>Component <EditPageLink editLink={EDIT_PAGE_LINK} /></h1>
         <p>Where is this component used?</p>
 
-        <InstallationSection url="demoComponentName" />
+        <InstallationSection url="demo-component-name" />
 
         <h1>Examples</h1>
         <h2>Full width</h2>
@@ -179,7 +197,7 @@ const TEMPLATE_PAGE = (
         <h1>Props</h1>
         <PropsTable data={temlateData} />
     </div>
-)
+);
 
 const contributeExample = {
     component: (
@@ -192,6 +210,6 @@ const contributeExample = {
     ),
     code: mycode,
     display: 'inline-block'
-}
+};
 
 export default ContributePage
--- packages/App/src/components/Pages/HomePage.js
+++ packages/App/src/components/Pages/HomePage.js
@@ -35,13 +35,13 @@ const HomePage = () => (
         </AntdHeader>
         <SectionHeader position="left" size="32px">About MyBit <EditPageLink editLink={EDIT_PAGE} /></SectionHeader>
         <p style={{textAlign: "justified"}}>
-        MyBit was started by a team of industry veterans who spotted a massive problem with today’s financial systems. Third party agents such as lawyers, escrow agents, and brokers are required to establish trust and conduct transactions.
-This is inefficient, unsafe, and expensive. Our vision is to utilise smart contract technology to eliminate the need for these centralised sources and creating a more efficient, secure, and cost-effective wealth management environment.
-MyBit provides robust developer tools such as SDKs and UI Kits to give engineers the power to rapidly build, test and deploy wealth management applications on the Ethereum blockchain as well as funding mechanisms to support and scale disruptive projects.
+            MyBit was started by a team of industry veterans who spotted a massive problem with today’s financial systems. Third party agents such as lawyers, escrow agents, and brokers are required to establish trust and conduct transactions.
+            This is inefficient, unsafe, and expensive. Our vision is to utilise smart contract technology to eliminate the need for these centralised sources and creating a more efficient, secure, and cost-effective wealth management environment.
+            MyBit provides robust developer tools such as SDKs and UI Kits to give engineers the power to rapidly build, test and deploy wealth management applications on the Ethereum blockchain as well as funding mechanisms to support and scale disruptive projects.
         </p>
         <p>
-        MyBit believes in the concept of co-creation. Community members guide our development with their advice, dedication and active participation in 
-        all aspects of the project. Join our community and get involved in our shared mission: to lead the way in the automated era. 
+            MyBit believes in the concept of co-creation. Community members guide our development with their advice, dedication and active participation in
+            all aspects of the project. Join our community and get involved in our shared mission: to lead the way in the automated era.
         </p>
 
         <SectionHeader position="left" size="32px">Installation</SectionHeader>
@@ -49,36 +49,60 @@ MyBit provides robust developer tools such as SDKs and UI Kits to give engineers
         <PlainCode>{`$ npm install --package-name--`}</PlainCode>
         <PlainCode>{`$ yarn add --package-name--`}</PlainCode>
         <h4>Usage</h4>
-        <PlainCode>{`import { --your-component-- } from '--package-name--'`}</PlainCode>
+        <PlainCode>{`import --your-component-- from '--package-name--'`}</PlainCode>
         <p>Import the antd stylesheet manually.</p>
         <PlainCode>{`import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'`}</PlainCode>
         <h4>Project structure</h4>
         <PlainCode>
-{`├── __tests__
-│   ├── *.test.js //test files
-├── public
-│   ├── favicon.png
-│   ├── index.html
-│   ├── manifest.json
-├── src
-│   ├── components
-│   │   ├── Common //components which build the library
-│   │   ├── Documentation
-│   │   │   ├── * //a folder for each documentation page
-│   │   │   ├── categories.js //a list of component categories
-│   │   │   ├── index.js //export documentation routes
-│   │   ├── Pages //the content section for each static page (Home, Contribute, etc)
-│   │   ├── Sections
-│   │   │   ├── Footer.js Header.js Sidebar.js
-│   │   │   ├── Content.js //contains the Router switch
-│   │   ├── App.js
-│   ├── showcase //folder to be publised as an NPM package
-│   │   ├── * //a folder for each isolated component
-│   │   │   ├── index.js //component's code
-│   │   │   ├── styled.js //styled-components
-│   ├── index.css //main styles
-│   ├── index.js
+{
+`
+├── .storybook
+│   ├── addons.js
+│   ├── config.js
+│
+├── Sample // sample package that can be used as a starting point while creating new component.
+│   ├── lib
+│   │   ├── index.js //component's code
+│   ├── .babelrc
+│   ├── .eslintrc
+│   ├── package.json
+│
+├── .babelrc
+├── lerna.json
 ├── package.json
+│
+├── packages
+│   ├── index.stories.js //the file containing storybook stories
+│   │
+│   ├── * //a folder for each isolated component as a npm package
+│   │   ├── lib
+│   │   │   ├── index.js //component's code
+│   │   ├── .babelrc
+│   │   ├── .eslintrc
+│   │   ├── package.json
+│   │
+│   ├── App // CRA app containing the documentation of the components
+│   │   ├── __tests__
+│   │   │   ├── *.test.js //test files
+│   │   ├── public
+│   │   │   ├── favicon.png
+│   │   │   ├── index.html
+│   │   │   ├── manifest.json
+│   │   ├── src
+│   │   │   ├── components
+│   │   │   │   ├── Common //components which build the library
+│   │   │   │   ├── Documentation
+│   │   │   │   │   ├── * //a folder for each documentation page
+│   │   │   │   │   ├── categories.js //a list of component categories
+│   │   │   │   │   ├── index.js //export documentation routes
+│   │   │   │   ├── Pages //the content section for each static page (Home, Contribute, etc)
+│   │   │   │   ├── Sections
+│   │   │   │   │   ├── Footer.js Header.js Sidebar.js
+│   │   │   │   │   ├── Content.js //contains the Router switch
+│   │   │   │   ├── App.js
+│   │   │   ├── index.css //main styles
+│   │   │   ├── index.js
+│   │   ├── package.json
 `}
         </PlainCode>
 
@@ -86,7 +110,7 @@ MyBit provides robust developer tools such as SDKs and UI Kits to give engineers
         <p>MyBit Foundation. Dammstrasse 16, 6300 Zug, Switzerland.</p>
         <p>MyBit Stiftung (MyBit Foundation) is registered in Zug, Switzerland.</p>
         <p>Identification number CHE-177.186.963 (pdf), Commercial register (web).</p>
-        
+
     </div>
 )
 
