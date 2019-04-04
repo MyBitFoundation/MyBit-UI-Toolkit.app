6a125ce01b1f7e6f0cb61d9bf5d2879f7534964b
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2019-04-03 22:45:18 -0600

    Removing global styles that should be included in the app and not the component

--- packages/Theme-Website/lib/index.js
+++ packages/Theme-Website/lib/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import stylesheet from './css/main.css';
 import tachyons from './css/tachyons-mybit.css';
 
-const ThemeWebsite = () => (<>
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-    <style dangerouslySetInnerHTML={{ __html: tachyons }} />
-</>);
+const ThemeWebsite = () => <style dangerouslySetInnerHTML={{ __html: tachyons }} />
 
 export default ThemeWebsite;
